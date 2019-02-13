516f91cfe5e64c6d03cdd56569d918a188cc783e
12826007 <kengo_tsunoda18@yahoo.co.jp>
2019-02-13 16:28:39 +0900

    Githubとの連携テスト

--- app/src/main/java/com/example/a12826007/schedulebook/MainActivity.java
+++ app/src/main/java/com/example/a12826007/schedulebook/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a12826007.schedulebook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,7 +20,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RealmTestActivity.class);
                 startActivity(intent);
+                int a = TestGithub(3);
+                Log.d("テスト結果：", String.valueOf(a) );
             }
         });
+    }
+
+    public int TestGithub(int a){
+        return a+3;
     }
 }
